b8ebf423e27f91d6678c16ded61f8d113302d203
Kamal <kamal.1@netprophetsglobal.com>
2023-03-12 13:41:59 +0530

    Update otp-history.js

--- api/routes/otp-history.js
+++ api/routes/otp-history.js
@@ -1,42 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const otpGenerator = require('otp-generator');
+// const otpGenerator = require('otp-generator');
 const otpModel = require('../models/otpModel');
+const validation = require('../validation/validateRequest')
+const {sendOTP} = require('../controller/appController')
 
-router.post('/send', (req, res, next) => {
-    var newOTP = Math.floor(100000 + Math.random() * 900000);
-    function randomString(length, chars) {
-        var result = '';
-        for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-        return result;
-    }
-    var rString = randomString(25, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-    var responseData = {};
-    const otp_model = new otpModel({
-        _id: new mongoose.Types.ObjectId(),
-        mobileNo: req.body.mobileNo,
-        otp: newOTP,
-        unsuccessfulAttempt: 0,
-        createdTime: new Date(),
-        otpExpiryTime: new Date(),
-        userBlockTime: new Date(),
-        resendOtpCount: 0,
-        txnId: rString
 
-    });
-    otp_model.save().then(result => {
-        console.log(result);
-        responseData["otp"] = otp_model.otp;
-        responseData["txnId"] = otp_model.txnId;
-        res.status(200).json({
-            code: 'APP001',
-            message: 'Request processed successfully',
-            data: responseData
-        });
-    }).catch(err => console.log(err));
+router.post('/send', validation.validReq,sendOTP);
 
-});
+// router.post('/send', (req, res, next) => {
+
+
+//     var newOTP = Math.floor(100000 + Math.random() * 900000);
+//     function randomString(length, chars) {
+//         var result = '';
+//         for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+//         return result;
+//     }
+//     var rString = randomString(25, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+//     var responseData = {};
+//     const otp_model = new otpModel({
+//         _id: new mongoose.Types.ObjectId(),
+//         mobileNo: req.body.mobileNo,
+//         otp: newOTP,
+//         unsuccessfulAttempt: 0,
+//         createdTime: new Date(),
+//         otpExpiryTime: new Date(),
+//         userBlockTime: new Date(),
+//         resendOtpCount: 0,
+//         txnId: rString
+
+//     });
+
+//     otp_model.save().then(result => {
+//         console.log(result);
+//         responseData["otp"] = otp_model.otp;
+//         responseData["txnId"] = otp_model.txnId;
+//         res.status(200).json({
+//             code: 'APP001',
+//             message: 'Request processed successfully',
+//             data: responseData
+//         });
+//     }).catch(err => console.log(err));
+
+// });
 
 router.post('/resend', (req, res, next) => {
     const txnId = req.body.txnId;
@@ -61,7 +69,7 @@ router.post('/resend', (req, res, next) => {
                     }).catch(err => console.log(err));
             }
             else {
-                res.status(200).json({code: 'APP904', message: 'No valid entry found for provided ID' });
+                res.status(200).json({ code: 'APP904', message: 'No valid entry found for provided ID' });
             }
         })
         .catch(err => {
